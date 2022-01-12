d20aa25c260bb2c489bbf2fb920447f665843da6
pblgllgs <pbl.gllgs@gmail.com>
2022-01-12 12:14:15 -0300

    update navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,52 +1,48 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+  const handleLogout = () => {
+    //TODO: por hacer
+    console.log("logout");
+  };
 
-            <Link
-                className="navbar-brand"
-                to="/"
-            >
-                Asociaciones
-            </Link>
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Asociaciones
+      </Link>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link " + (isActive ? "active" : "")
+            }
+            to="/marvel"
+          >
+            Marvel
+          </NavLink>
 
-                    <NavLink
-                        activeClassName="active"
-                        className="nav-item nav-link"
-                        exact
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link " + (isActive ? "active" : "")
+            }
+            to="/dc"
+          >
+            DC
+          </NavLink>
+        </div>
+      </div>
 
-                    <NavLink
-                        activeClassName="active"
-                        className="nav-item nav-link"
-                        exact
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-                </div>
-            </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
-                    <NavLink
-                        activeClassName="active"
-                        className="nav-item nav-link"
-                        exact
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link text-info">Pablo</span>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,11 +4,12 @@ import { DcScreen } from "../components/dc/DcScreen";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { MarvelScreen } from "../components/marvel/MarvelScreen";
 import { SearchScreen } from "../components/search/SearchScreen";
+import { Navbar } from "../components/ui/Navbar";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      <h1>Welcome to React Router!</h1>
+    <Navbar />
       <Routes>
         <Route path="/" element={<MarvelScreen />} />
         <Route path="/marvel" element={<MarvelScreen />} />
