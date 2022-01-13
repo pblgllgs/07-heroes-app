97e0b4af512cfe5429f1196565ada4f9341f796d
pblgllgs <pbl.gllgs@gmail.com>
2022-01-13 12:27:11 -0300

    component HeroCard

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectores/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <div className='card-columns'>
+        <div className='row rows-cols-1 row-cols-3 g-3'>
 
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard
+                        key={hero.id}
+                        { ...hero } />
                 ))
             }
 
