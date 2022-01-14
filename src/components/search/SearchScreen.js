7aa2f89294e3680fd9ce1af9ff85c2aa56d6101e
pblgllgs <pbl.gllgs@gmail.com>
2022-01-14 13:37:37 -0300

    implement useForm hook

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm-';
 
 export const SearchScreen = () => {
+
+    const initialState= {
+        searchText: ''
+    }
+    const [{searchText}, handleInputChange, reset] = useForm(initialState);
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        console.log(searchText)
+        reset();
+    }
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <h1>Busqueda</h1>
+            <hr />
+            <div className='row'>
+                <div className='col-5'>
+                    <h4>Buscar</h4>
+                    <hr />
+                    <form onSubmit={handleSearch}>
+                        <input
+                            type='text'
+                            placeholder='Ingresa un id'
+                            className='form-control'
+                            name='searchText'
+                            autoComplete='off'
+                            value={searchText}
+                            onChange={handleInputChange}
+                        />
+                        <button className='btn btn-primary mt-3' type='submit'>
+                            Buscar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -36,6 +36,14 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link " + (isActive ? "active" : "")
+            }
+            to="/search"
+          >
+            Search
+          </NavLink>
         </div>
       </div>
 
