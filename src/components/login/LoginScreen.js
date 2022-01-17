b2d471b9aae76b242f2f1e323e64afc826ebcdb6
pblgllgs <pbl.gllgs@gmail.com>
2022-01-17 12:12:57 -0300

    update

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { types } from "../../types/types";
 import { AuthContext } from "../../auth/authContext";
