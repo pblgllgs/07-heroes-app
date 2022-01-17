4efd8f2adf1fda5b1ca3d410b6d30ae70ea46791
pblgllgs <pbl.gllgs@gmail.com>
2022-01-17 10:19:06 -0300

    vista de la busqueda

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,43 +1,64 @@
-import React from 'react'
-import { useForm } from '../../hooks/useForm-';
+import React, { useMemo } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useForm } from "../../hooks/useForm-";
+import { getHeroesByName } from "../../selectores/getHeroesByName";
+import { HeroCard } from "../hero/HeroCard";
+import queryString from "query-string";
 
 export const SearchScreen = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
 
-    const initialState= {
-        searchText: ''
-    }
-    const [{searchText}, handleInputChange, reset] = useForm(initialState);
+    const { q = '' } = queryString.parse(location.search);
+
+    const initialState = {
+        searchText: q,
+    };
+    const [{ searchText }, handleInputChange, reset] = useForm(initialState);
+
+    const heroesFiltrados = useMemo(() => getHeroesByName(q),[q]);
 
     const handleSearch = (e) => {
-        e.preventDefault()
-        console.log(searchText)
-        reset();
-    }
+        e.preventDefault();
+        navigate(`?q=${searchText}`);
+    };
 
     return (
         <>
             <h1>Busqueda</h1>
             <hr />
-            <div className='row'>
-                <div className='col-5'>
+            <div className="row">
+                <div className="col-5">
                     <h4>Buscar</h4>
                     <hr />
                     <form onSubmit={handleSearch}>
                         <input
-                            type='text'
-                            placeholder='Ingresa un id'
-                            className='form-control'
-                            name='searchText'
-                            autoComplete='off'
+                            type="text"
+                            placeholder="Ingresa un nombre..."
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
                             value={searchText}
                             onChange={handleInputChange}
                         />
-                        <button className='btn btn-primary mt-3' type='submit'>
+                        <button className="btn btn-primary mt-3" type="submit">
                             Buscar
                         </button>
                     </form>
                 </div>
+                <div className="col-7">
+                    <h4>Resultados</h4>
+                    <hr />
+                    {
+                        (q === '')
+                        ? <div className="alert alert-info">Acá se mostrarán las coincidencias</div>
+                        : (heroesFiltrados.length === 0 ) && <div className="alert alert-danger">No hay resultados para {q}</div>
+                    }
+                    {heroesFiltrados.map((heroe) => (
+                        <HeroCard key={heroe.id} {...heroe} />
+                    ))}
+                </div>
             </div>
         </>
-    )
-}
+    );
+};
