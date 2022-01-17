b7f953ce6d5e0a84b0d584cacc8aec0235816f88
pblgllgs <pbl.gllgs@gmail.com>
2022-01-17 15:39:04 -0300

    update router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,16 +2,32 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
-
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginScreen />} />
+    return (
+        <BrowserRouter>
+            <Routes>
 
-        <Route path="/*" element={<DashboardRoutes />} />
-      </Routes>
-    </BrowserRouter>
-  );
+                <Route
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <LoginScreen />
+                        </PublicRoute>
+                    }
+                />
+
+                <Route
+                    path="/*"
+                    element={
+                        <PrivateRoute>
+                            <DashboardRoutes />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 };
