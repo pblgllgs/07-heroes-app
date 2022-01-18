c06eb6351f1c679fd5f7d3b95cf97107b86b05fb
pblgllgs <pbl.gllgs@gmail.com>
2022-01-18 10:20:22 -0300

    fix

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -27,7 +27,7 @@ export const LoginScreen = () => {
 
     return (
         <div className="container mt-5">
-            <h1>LoginScreen</h1>
+            <h1>Login</h1>
             <hr />
             <button className="btn btn-primary" onClick={handleLogin}>
                 Login
