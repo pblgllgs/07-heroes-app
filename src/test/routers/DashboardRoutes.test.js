52f4f5c3309d9bc54a07cd198c13e17e88baecac
pblgllgs <pbl.gllgs@gmail.com>
2022-01-19 12:28:13 -0300

    test DashboardRoutes

---
+++ src/test/routers/DashboardRoutes.test.js
@@ -0,0 +1,49 @@
+import { MemoryRouter } from "react-router-dom";
+const { mount } = require("enzyme");
+const { AuthContext } = require("../../auth/authContext");
+const { DashboardRoutes } = require("../../routers/DashboardRoutes");
+
+describe("Pruebas en <DashboardRoutes />", () => {
+    //creamos un contexto valido para rutas privadas
+    const contextValue = {
+        user: {
+            name: "Pablo",
+            logged: true,
+        },
+    };
+
+    //MemoryRouter ayuda a proveer el useNAvigate
+    test("Debe de mostrarse correctamente - Marvel", () => {
+        //capturamos el componente montado, paramos el contexto
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/']}>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+        //comparamos con el snapshot
+        expect(wrapper).toMatchSnapshot();
+        //esperamos que dentro del componente se encuentre un elemento text-info con el valor de Pablo
+        expect(wrapper.find(".text-info").text().trim()).toBe("Pablo");
+        expect(wrapper.find("h1").text().trim()).toBe("Marvel Heroes");
+    });
+
+    test("Debe de mostrarse correctamente - DC", () => {
+        //capturamos el componente montado, paramos el contexto
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={["/dc"]}>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+        console.log(wrapper.html());
+        
+        //comparamos con el snapshot
+        expect(wrapper).toMatchSnapshot();
+        //esperamos que dentro del componente se encuentre un elemento text-info con el valor de Pablo
+        expect(wrapper.find(".text-info").text().trim()).toBe("Pablo");
+        expect(wrapper.find("h1").text().trim()).toBe("DC Heroes");
+    });
+});
