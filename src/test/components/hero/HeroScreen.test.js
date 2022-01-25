0155042b4bd8f60676ef570a01a9547f0cc91c0a
pblgllgs <pbl.gllgs@gmail.com>
2022-01-25 10:31:17 -0300

    pruebas en el HeroScreen memoryRouter, Route, mockNavigate

--- src/test/components/hero/HeroScreen.test.js
+++ src/test/components/hero/HeroScreen.test.js
@@ -17,16 +17,15 @@ describe("pruebas en <HeroScreen />", () => {
             <MemoryRouter initialEntries={["/hero"]}>
                 <Routes>
                     <Route path="/hero" element={<HeroScreen />} />
-                    <Route path="/" element={<h1>No hero page</h1>} />
+                    <Route path="/" element={<h1>No Hero page</h1>} />
                 </Routes>
             </MemoryRouter>
         );
         //esoeramos que al no tener un heroe en el url se muestre la pagina de no hero page
-        expect(wrapper.find("h1").text().trim()).toBe("No hero page");
+        expect(wrapper.find("h1").text().trim()).toBe("No Hero page");
     });
 
     test("debe de mostrar HeroScreen si el parametro existe y se encuentra", () => {
-
         //utilizamos un id existente
         const id = "marvel-spider";
         const wrapper = mount(
@@ -34,13 +33,38 @@ describe("pruebas en <HeroScreen />", () => {
             <MemoryRouter initialEntries={[`/hero/${id}`]}>
                 <Routes>
                     <Route path="/hero/:heroeId" element={<HeroScreen />} />
-                    <Route path="/" element={<h1>No hero page</h1>} />
+                    <Route path="/" element={<h1>No Hero page</h1>} />
                 </Routes>
             </MemoryRouter>
         );
         //utilizamos el elemento img y sacamos su props de src
-        const src = wrapper.find('img').prop('src');
+        const src = wrapper.find("img").prop("src");
         //esperamos que src de la imagen contenga el id del heroe
         expect(src).toContain(id);
     });
+
+    test("debe de regresar a la pantalla anterior", () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={["/hero/marvel-spider"]}>
+                <Routes>
+                    <Route path="/hero/:heroeId" element={<HeroScreen />} />
+                </Routes>
+            </MemoryRouter>
+        );
+        wrapper.find('button').prop('onClick')();
+        expect(mockNavigate).toHaveBeenCalledWith(-1);
+    });
+
+    test("debe de mostrar el no hero page si no tenemos un heroe", () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={["/hero/CUALQUIER_COSA"]}>
+                <Routes>
+                    <Route path="/hero/:heroeId" element={<HeroScreen />} />
+                    <Route path="/" element={<h1>No Hero page</h1>} />
+                </Routes>
+            </MemoryRouter>
+        );
+        expect(wrapper.text()).toBe("No Hero page");
+    });
+    
 });
